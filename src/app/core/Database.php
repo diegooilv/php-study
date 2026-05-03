b01e05defeb8a1050dacffdc9525ecb33edd04a1
Diego <diego.oliv2007@gmail.com>
2026-05-03 17:36:36 -0300

    fix: database debug error codes

--- src/app/core/Database.php
+++ src/app/core/Database.php
@@ -2,14 +2,15 @@
 class Database
 {
     private static $pdo;
-
-
     public static function connect()
     {
         if (!self::$pdo) {
             $config = require __DIR__ . "/../../config/database.php";
             $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
-            self::$pdo = new PDO($dsn, $config['user'], $config['pass']);
+            self::$pdo = new PDO($dsn, $config['user'], $config['pass'], [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
         }
 
         return self::$pdo;
